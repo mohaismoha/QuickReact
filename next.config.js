4c78274c7e10a1734c800b048182c23603e920f6
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-10-12 00:01:21 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push("pino-pretty", "lokijs", "encoding")
+    return config
+  },
+}
+
+module.exports = nextConfig
