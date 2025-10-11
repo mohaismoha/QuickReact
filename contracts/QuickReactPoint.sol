0e99c8467a9cbd4c2815df941921601d1f9b88bc
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-10-11 23:53:16 +0100

    Update QuickReactPoint.sol

--- contracts/QuickReactPoint.sol
+++ contracts/QuickReactPoint.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract QuickReactPoint is ERC20, Ownable {
+    address public minter;
+
+    constructor() ERC20("QuickReactPoint", "QRP") Ownable(msg.sender) {}
+
+    function setMinter(address _minter) external onlyOwner {
+        minter = _minter;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == minter, "Not minter");
+        _mint(to, amount);
+    }
+}
